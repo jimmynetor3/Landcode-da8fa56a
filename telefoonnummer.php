90caa584fba995102e38730c66417bbb6db58a20
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-06-19 11:21:15 +0200

    af

--- telefoonnummer.php
+++ telefoonnummer.php
@@ -1,20 +1,22 @@
 <?php
 
-
+$input = $argv[1];
 $pattern = '/[+][0-9]{11}/m';
 $pattern2 = '/0[0-9]{9}/m';
-$pattern3 = '/[+]316[0-9]{8}/m';
-$input = $argv[1];
-if (preg_match($pattern, $input) || preg_match($pattern2, $input) || preg_match($pattern3, $input)) {
-    echo("$input is een geldig telefoonnummer" . PHP_EOL);
-} else {
-    echo("$input is niet een geldig telefoonnummer" . PHP_EOL);
+$landcode = '/[+]{1}[0-9]{2}/m';
+//$landcode2 = '/[+]{1}[0-9]{2}/m';
+
+
+if (preg_match($pattern, $input, $matches) || preg_match($pattern2, $input, $matches)) {
+    if ($matches[0] === $input) {
+        echo("$input is een geldig telefoonnummer" . PHP_EOL);
+    } else {
+        echo("is niet een geldig telefoonnummer" . PHP_EOL);
+    }
 }
 
-if (preg_match('/[+]{1}[0-9]{2}/m', $input, $matches)) {
+if (preg_match($landcode, $input, $matches)) {
     print_r($matches[0] . " is de landcode");
-//    $StringArray = str_split($input);
-//    print_r($StringArray[0] . $StringArray[1] . $StringArray[2]);
 } else {
     echo("no match");
 }
